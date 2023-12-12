5d49237346eef92ea6773ecd7a13db9897cab255
Serhii <serikgorbanev@gmail.com>
2023-12-13 00:33:52 +0100

    Changed Health's actions for UnityEvent's. Also added tooltips

--- Assets/Scripts/Health And Damage/Health.cs
+++ Assets/Scripts/Health And Damage/Health.cs
@@ -2,20 +2,26 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace HealthAndDamage
 {
     public class Health : MonoBehaviour, IDamageable
     {
-        public float health { get; set; }
+        [SerializeField] public float health;
 
-        Action<Damage> OnDamage { get; set; }
+        [Tooltip("actions that will be performed when player should die")]
+        [SerializeField] UnityEvent<Damage> OnDeath;
 
-        Dictionary<DamageType, float> damageTypeMultipliers { get; set; }
+        [Tooltip("actions that will be performed when taking damage")]
+        [SerializeField] UnityEvent<Damage> OnDamage;
+
+        [Tooltip("keys are the damage types, and values are the multipliers. when taking damage the damage will be multiplied by value with the key that matches with the damage type. if it doesn't match with any keys it won't be multiplied by anything")]
+        [SerializeField] Dictionary<DamageType, float> damageTypeMultipliers;
 
         public void ApplyDamage(Damage damage)
         {
-            OnDamage(damage);
+            OnDamage.Invoke(damage);
 
             float damageToDeal = damage.damageValue;
 
@@ -30,6 +36,5 @@ namespace HealthAndDamage
         {
             health += healValue;
         }
-        public abstract void Death();
     }
 }
