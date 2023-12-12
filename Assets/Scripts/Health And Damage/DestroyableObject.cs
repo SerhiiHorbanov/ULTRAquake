d700f049e8461157352a8d26977ce87b32cda008
Serhii <serikgorbanev@gmail.com>
2023-12-13 00:14:24 +0100

    Added DestroyableObject component
    it can be used for breakable walls and other things, and it's also very easy to set up

---
+++ Assets/Scripts/Health And Damage/DestroyableObject.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace HealthAndDamage
+{
+    public class DestroyableObject : MonoBehaviour, IDamageable
+    {
+        [SerializeField] UnityEvent<Damage> OnDamage;
+
+        [Tooltip("types of damage that can destroy that object. if empty, damage of any type will destroy the object")]
+        [SerializeField] List<DamageType> destroyingTypes = new List<DamageType>();
+
+        public void ApplyDamage(Damage damage)
+        {
+            OnDamage.Invoke(damage);
+
+            if (destroyingTypes == null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+
+            if (destroyingTypes.Contains(damage.type) || destroyingTypes.Count == 0)
+                Destroy(gameObject);
+        }
+    }
+}
