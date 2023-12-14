05c3ce1576ec51ec5e9f1ecd67d59f5b0a7ff72d
Serhii <serikgorbanev@gmail.com>
2023-12-14 01:04:58 +0100

    Changed WeaponTypeData.
    Now it's protected from changes from other classes. Also added some new fields

--- Assets/Scripts/Weapons/WeaponTypeData.cs
+++ Assets/Scripts/Weapons/WeaponTypeData.cs
@@ -1,3 +1,4 @@
+using HealthAndDamage;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -7,12 +8,28 @@ namespace Weapons
     [CreateAssetMenu()]
     public class WeaponTypeData : ScriptableObject
     {
-        public string type;
+        [Tooltip("basically identificator for the weapon. if you pick up 2 weapons with the same name, the new one won't be added to your weapons but you will get the ammo")]
+        [SerializeField] private string weaponName;
 
-        public int maxAmmo;
+        [Header("Ammo")]
+        [SerializeField] private bool isAmmoInfinite;
+        [SerializeField] private int ammoPerShot;
 
-        public bool isAmmoInfinite;
+        [Header("Shooting")]
+        [SerializeField] private float scatter;//in degrees (Celsius)
+        [SerializeField] private int shotsPerAttack;
+        [SerializeField] private Vector2[] pattern;//in degrees (Celsius)
+        [SerializeField] private Damage damage;
 
-        public GameObject weaponVisualsPrefab;
+        [SerializeField] private GameObject weaponVisualsPrefab;
+
+        public string WeaponName => weaponName;
+        public bool IsAmmoInfinite => isAmmoInfinite;
+        public int AmmoPerShot => ammoPerShot;
+        public float Scatter => scatter;
+        public int ShotsPerAttack => shotsPerAttack;
+        public Vector2[] Pattern => pattern;
+        public Damage Damage => damage;
+        public GameObject WeaponVisualsPrefab => weaponVisualsPrefab;
     }
 }
