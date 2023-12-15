3a799644f9761e40a5c3ebc6d2e7cf8491f61bb0
Serhii <serikgorbanev@gmail.com>
2023-12-15 19:57:48 +0100

    Added AmmoManager

---
+++ Assets/Scripts/Weapons/AmmoManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Weapons;
+
+public class AmmoManager : MonoBehaviour
+{
+    [SerializeField] SerializableDictionary<AmmoTypes, int> ammo;
+    [SerializeField] SerializableDictionary<AmmoTypes, int> maxAmmo;
+
+    public void AddAmmo(AmmoTypes type, int amount)
+    {
+        int clampedAmmoAmount = Mathf.Clamp(amount, 0, GetMaxAmmo(type));
+
+        if (ammo == null)
+            ammo = new SerializableDictionary<AmmoTypes, int>() { { type, clampedAmmoAmount } };
+
+        else if (ammo.ContainsKey(type))
+        {
+            ammo[type] += amount;
+            ammo[type] = Mathf.Clamp(ammo[type], 0, GetMaxAmmo(type));
+        }
+
+        else
+            ammo.Add(type, clampedAmmoAmount);
+    }
+
+    public int GetAmmo(AmmoTypes type)
+    {
+        if (ammo == null)
+            ammo = new SerializableDictionary<AmmoTypes, int>() { { type, 0 } };
+
+        else if (ammo.ContainsKey(type))
+            return ammo[type];
+
+        else
+            ammo = new SerializableDictionary<AmmoTypes, int>() { { type, 0 } };
+
+        return 0;
+    }
+
+    public int GetMaxAmmo(AmmoTypes type)
+    {
+        if (maxAmmo == null)
+            return 0;
+
+        if (maxAmmo.ContainsKey(type))
+            return maxAmmo[type];
+
+        return 0;
+    }
+}
