4936aa26d24960db43d86bbf5b6bed099b40ee8e
Serhii <serikgorbanev@gmail.com>
2023-12-19 11:30:44 +0100

    Moved projectile based weapons related scripts to new Projectile folder
    Also moved their classes to Weapons.ProjectileBased

--- Assets/Scripts/Weapons/Ammo/AmmoTypes.cs
+++ Assets/Scripts/Weapons/Ammo/AmmoTypes.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-namespace Weapons
+
+namespace Weapons.Ammo
 {
     public enum AmmoTypes
     {
--- Assets/Scripts/Weapons/ProjectileBased/ProjectileBasedWeaponTypeData.cs
+++ Assets/Scripts/Weapons/ProjectileBased/ProjectileBasedWeaponTypeData.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Weapons;
 
-namespace Weapons
+namespace Weapons.ProjectileBased
 {
     [CreateAssetMenu()]
     public class ProjectileBasedWeaponTypeData : WeaponTypeData
