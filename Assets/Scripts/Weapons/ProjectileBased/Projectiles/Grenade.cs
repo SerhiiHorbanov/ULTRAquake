f8bbb3bb0f20b6d7427caba1525e5d5a82d6c70b
Serhii <serikgorbanev@gmail.com>
2023-12-21 14:31:37 +0100

    Made Grenade component

--- Assets/Scripts/Weapons/ProjectileBased/Projectiles/Grenade.cs
+++ Assets/Scripts/Weapons/ProjectileBased/Projectiles/Grenade.cs
@@ -1,3 +1,4 @@
+using HealthAndDamage;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,50 @@ namespace Weapons.ProjectileBased.Projectiles
 {
     public class Grenade : MonoBehaviour
     {
-        private void OnTriggerEnter(Collider other)
+        [SerializeField] private int explosionRadius;
+        [SerializeField] private int explosionPower;
+        [SerializeField] private Projectile projectileComponent;
+
+        private void Awake()
         {
-            Destroy(gameObject);
+            
+        }
+
+        private void Explode()
+        {
+            Collider[] affectedByExplosion = Physics.OverlapSphere(transform.position, explosionRadius);
+            foreach (Collider collider in affectedByExplosion)
+            {
+                Rigidbody affectedRigidBody = collider.GetComponent<Rigidbody>();
+                IDamageable iDamageable = collider.GetComponent<IDamageable>();
+
+                if (affectedRigidBody != null)
+                    AddExplosionForce(affectedRigidBody, collider);
+
+                if (iDamageable != null)
+                    iDamageable.ApplyDamage(projectileComponent.damage);
+            }
+        }
+
+        private void AddExplosionForce(Rigidbody rigidbody, Collider collider)
+        {
+            Vector3 affectedObjectRelativePosition = collider.ClosestPoint(transform.position) - transform.position;
+            Vector3 direction = affectedObjectRelativePosition.normalized;
+
+            float forceMagnitude = 1f / (affectedObjectRelativePosition.magnitude + 1);
+
+            rigidbody.AddForce(direction * forceMagnitude);
+        }
+
+        private void OnDestroy()
+        {
+            Explode();
+        }
+
+        private void OnTriggerEnter(Collider collider)
+        {
+            if (collider.GetComponent<Health>() != null)
+                Destroy(gameObject);
         }
     }
 }
