73bcd7794020cfdab8bfc53b45858b3c3ded58dc
Serhii <serikgorbanev@gmail.com>
2023-12-21 16:05:23 +0100

    Added operators and constructor to Damage

--- Assets/Scripts/Health And Damage/Damage.cs
+++ Assets/Scripts/Health And Damage/Damage.cs
@@ -8,5 +8,32 @@ namespace HealthAndDamage
         public float damageValue;
         public DamageType type;
         //also we need some way of identifying who dealt the damage but i'm not sure how
+
+        public Damage(float damageValue, DamageType type)
+        {
+            this.damageValue = damageValue;
+            this.type = type;
+        }
+
+        public static Damage operator *(Damage damage, float multiplier)
+        {
+            damage.damageValue *= multiplier;
+            return damage;
+        }
+        public static Damage operator /(Damage damage, float denominator)
+        {
+            damage.damageValue /= denominator;
+            return damage;
+        }
+        public static Damage operator +(Damage damage, float addition)
+        {
+            damage.damageValue += addition;
+            return damage;
+        }
+        public static Damage operator -(Damage damage, float subtractor)
+        {
+            damage.damageValue -= subtractor;
+            return damage;
+        }
     }
 }
