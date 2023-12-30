6a5ec94920ba40e8fc115bd0015f02fc642df79a
Serhii <serikgorbanev@gmail.com>
2023-12-30 09:52:06 +0100

    Fixed Camera bug
    before fix, it could be rotated upside down if you look up or down

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -7,13 +7,23 @@ namespace Player
         [SerializeField] Transform cameraTransform;
 
         [SerializeField] float sensitivity;
-
+        [SerializeField] float maxXRotation = 90;//X axis rotation is up and down
+        [SerializeField] float minXRotation = -90;//X axis rotation is up and down
         public void MouseLook(Vector2 delta)
             => RotateLook(delta * sensitivity);
 
         public void RotateLook(Vector2 rotation)
         {
-            Vector3 newCameraRotationEuler = cameraTransform.localRotation.eulerAngles + new Vector3(-rotation.y, 0, 0);
+            Vector3 newCameraRotationEuler = cameraTransform.localRotation.eulerAngles;
+            newCameraRotationEuler.x += -rotation.y;
+
+            if (newCameraRotationEuler.x > 180)//it's for easier calculations
+                newCameraRotationEuler.x -= 360;
+
+            newCameraRotationEuler.x = Mathf.Clamp(newCameraRotationEuler.x, minXRotation, maxXRotation);
+
+            Debug.Log($"newCameraRotationEuler = {newCameraRotationEuler}");
+
             cameraTransform.localRotation = Quaternion.Euler(newCameraRotationEuler);
 
             transform.Rotate(0, rotation.x, 0);
